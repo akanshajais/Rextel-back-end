92c5357957c66ae05c8f73dbb8a7c9b5f9920e69
AkanshaJaiswal <akanshajaiswal663@gmail.com>
2022-01-19 11:18:06 +0545

    second commit

---
+++ src/index.server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const env = require('dotenv');
+const app  = express();
+
+const mongoose = require('mongoose');
+
+const  authRoutes = require('./routes/auth');
+const  adminRoutes = require('./routes/admin/auth');
+const categoryRoutes = require('./routes/category');
+env.config();
+//mongodb+srv://JaiswalAkansha:<password>@cluster0.db7af.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+const URL = `mongodb+srv://${username}:${password}@cluster0.0xjd4.mongodb.net/REXTEL?retryWrites=true&w=majority`;
+const Connection = async(username,password) =>{
+    try{
+        await mongoose.connect(URL,{useNewUrlParser:true, useUnifiedTopology:true});
+        console.log('database connected');
+    }catch(error)
+    {
+        console.log('ERROR: ',error.message);
+    }
+}
+
+Connection(username,password);
+app.use(express.json());
+app.use('/api',authRoutes);
+app.use('/api',adminRoutes);
+app.use('/api',categoryRoutes);
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is running on port ${process.env.PORT}`);
+});
